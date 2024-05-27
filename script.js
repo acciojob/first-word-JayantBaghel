ec715a54bdb920a2d5766f4f6717019a82c5c992
JayantBaghel <94191857+JayantBaghel@users.noreply.github.com>
2024-05-27 10:36:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
+	// Check if the string is empty
+    if (str.length === 0) {
+        return str;
+    }
+
+    // Trim leading spaces and find the index of the first space
+    let trimmedStr = str.trim();
+    let firstSpaceIndex = trimmedStr.indexOf(' ');
+
+    // If no space is found, return the entire string
+    if (firstSpaceIndex === -1) {
+        return trimmedStr;
+    }
+
+    // Return the substring from the beginning to the first space
+    return trimmedStr.substring(0, firstSpaceIndex);
 }
 
 // Do not change the code below
